dba39e2cbbe19e50b18baec8d9e90c77e8bb5eae
Abdul Wahab <abdul_wahab@elixir.com>
2020-10-20 02:54:51 +0500

    products table changes

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -12,7 +12,7 @@ const reviewSchema = new mongoose.Schema(
 );
 const prodctSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: String, required: false },
   brand: { type: String, required: true },
   price: { type: Number, default: 0, required: true },
   category: { type: String, required: true },
